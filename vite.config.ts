592e6ac73fbb38cd032f704ae1b8f548898fb95d
KimHyunJun-55 <mical154@naver.com>
2025-04-20 03:50:46 +0900

    ??

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/", // ✅ 반드시 추가
+  build: {
+    outDir: "dist", // ✅ 빌드 출력 폴더 명시
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: "./index.html", // ✅ 진입점 명시
+      },
+    },
+  },
+});
